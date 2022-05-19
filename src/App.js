8f46dc9528154560dfbe2caa17c3fb1c0c1f3ba1
rakibul114 <rakibulislam114@gmail.com>
2022-05-19 07:33:09 +0600

    finished the project

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import {  useEffect, useState } from 'react';
 import './App.css';
 import Form from './components/Form';
 import Header from './components/Header';
@@ -7,6 +7,17 @@ import TodosList from './components/TodosList';
 function App() {
   const [input, setInput] = useState('');
   const [todos, setTodos] = useState([]);
+  const [editTodo, setEditTodo] = useState(null);
+
+  useEffect(() => {
+    const lists = JSON.parse(localStorage.getItem("todos"));
+    if (todos) {
+     setTodos(lists);
+    }
+  }, [todos]);
+
+  localStorage.setItem("todos", JSON.stringify(todos));
+  
 
   return (
     <div className="container">
@@ -20,12 +31,16 @@ function App() {
             setInput={setInput}
             todos={todos}
             setTodos={setTodos}
+            editTodo={editTodo}
+            setEditTodo={setEditTodo}
           />
         </div>
         <div>
           <TodosList
             todos={todos}
-            setTodos={setTodos} />
+            setTodos={setTodos}
+            setEditTodo={setEditTodo}
+          />
         </div>
       </div>
     </div>
--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -1,17 +1,59 @@
 import React from 'react';
 
-const TodosList = ({ todos, setTodos }) => {
-    return (
+
+
+const TodosList = ({ todos, setTodos, setEditTodo }) => {
+
+    // Complete button
+    const handleComplete = ( todo ) => {
         
-        <div>
-        {
-            todos.map(todo => (
-                <li className='todo-list'>
-                
-                </li>
-            ))
-        }
-        </div>
+        setTodos(
+            todos.map((item) => {
+                if (item?.id === todo?.id) {
+                    return { ...item, completed: !item.completed };
+                }
+                return item;
+            })
+        );
+    };
+
+    // Edit button
+    const handleEdit = ({id}) => {
+        const findTodo = todos.find(todo => todo.id === id);
+        console.log(findTodo);
+        setEditTodo(findTodo);
+    };
+
+    // Delete button
+    const handleDelete = ({ id }) => {
+        setTodos(todos.filter(todo => todo.id !== id));
+    };
+
+    return (
+      <div>
+        {todos.map((todo) => (
+          <li className="list-item" key={todo.id}>
+            <input
+              type="text"
+              value={todo.title}
+              className={`list ${todo.completed ? 'complete' : ''}`}
+              onChange={(event) => event.preventDefault()}
+            />
+
+            <div>
+              <button className="button-complete task-button" onClick={() =>handleComplete(todo)}>
+                <i className="fa-solid fa-circle-check"></i>
+              </button>
+              <button className="button-edit task-button" onClick={() => handleEdit(todo)}>
+                <i className="fa-solid fa-pen-to-square"></i>
+              </button>
+              <button className="button-delete task-button" onClick={() =>handleDelete(todo)}>
+                <i className="fa-solid fa-trash-can"></i>
+              </button>
+            </div>
+          </li>
+        ))}
+      </div>
     );
 };
 
